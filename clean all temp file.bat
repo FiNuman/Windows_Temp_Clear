1fca3c47706235f98111ba317bdc2aea0c227482
Abdullah Al Numan <42403401+FiNuman@users.noreply.github.com>
2021-05-30 22:24:46 +0600

    Add files via upload

---
+++ clean all temp file.bat
@@ -0,0 +1,30 @@
+Code: - 
+@echo off
+del /s /f /q c:\windows\temp\*.*
+rd /s /q c:\windows\temp
+md c:\windows\temp
+del /s /f /q C:\WINDOWS\Prefetch
+del /s /f /q %temp%\*.*
+rd /s /q %temp%
+md %temp%
+deltree /y c:\windows\tempor~1
+deltree /y c:\windows\tempsss
+deltree /y c:\windows\tmp
+deltree /y c:\windows\ff*.tmp
+deltree /y c:\windows\prefetch
+deltree /y c:\windows\history
+deltree /y c:\windows\cookies
+deltree /y c:\windows\recent
+deltree /y c:\windows\spool\printers
+
+echo.
+echo Task completed successfully -------------
+echo Wait loading tree --------------
+TIMEOUT 3
+tree
+echo -------------------------------------------------------------------
+echo -------------------------------------------------------------------
+tree 
+echo -------------------------------------------------------------------
+echo -------------------------------------------------------------------
+tree 
